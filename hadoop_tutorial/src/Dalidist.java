9c223a21f530b8b53a8fbd062b0a0ce0a94b4fc5
yishingene <yishin0819@gmail.com>
2020-10-18 15:47:21 +0000

    add folder

--- hadoop_tutorial/src/Dalidist.java
+++ hadoop_tutorial/src/Dalidist.java
@@ -14,37 +14,38 @@ public class Dalidist {
         
  public static class Map extends Mapper<LongWritable, Text, Text, MapWritable> {
     private MapWritable arry = new MapWritable();
-    private Text keydate = new Text();
+    private Text word = new Text();
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
     
-    String line = value.toString();
-    StringTokenizer tokenizer = new StringTokenizer(line,",");
-    int position=0;
-    int count=0;
-    Float f = new Float("20.75f");
-    while (tokenizer.hasMoreTokens()) {
-        String token = tokenizer.nextToken();
-        position ++;
-        count++;
-        if (count==1){String keydate=token;}
-        Float tmpparse = f.parseFloat(token);
-        if (count==4){arry.put(new IntWritable(0),new FloatWritable(tmpparse));}
-        
-
-        
-    }
-    context.write(keydate, arry);
- 
-
+      String line = value.toString();
+      StringTokenizer tokenizer = new StringTokenizer(line,",");
+      int position=0;
+      int count=0;
+      
+      while (tokenizer.hasMoreTokens()) {
+          // String token = tokenizer.nextToken();
+          position ++;
+          count ++;
+          // if (count==1){String keydate=token;}
+          Double tmpparse = Double.parseDouble(tokenizer.nextToken());
+          if (count==4){
+            arry.put(new IntWritable(0),new DoubleWritable(tmpparse));
+            context.write(word, arry);
+          }   
+      }
  } 
 }       
- public static class Reduce extends Reducer<Text, MapWritable, Text, FloatWritable> {
+ public static class Reduce extends Reducer<Text, MapWritable, Text, DoubleWritable> {
     public void reduce(Text key, Iterable<MapWritable> arry, Context context) 
       throws IOException, InterruptedException {
+
+        
+        double tmp = 0.2;
         for (MapWritable ar : arry) {
-            System.out.println(ar);
-            Float tmp = ((FloatWritable)ar.get(new IntWritable(0))).get();
-            context.write(new Text(key), new FloatWritable(tmp));
+            // System.out.println(ar);
+            tmp = ((DoubleWritable)ar.get(new IntWritable(0))).get();
+            // String strtmp = String.valueOf(tmp);  
+            context.write(key, new DoubleWritable(tmp));
         }
   }
 }      
@@ -54,17 +55,28 @@ public class Dalidist {
         
         Job job = new Job(conf, "dalidist");
     
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    job.setMapOutputValueClass(MapWritable.class); // array 
-    job.setOutputValueClass(DoubleWritable.class); 
-    job.setOutputValueClass(FloatWritable.class);    
     job.setMapperClass(Map.class);
     job.setReducerClass(Reduce.class);
-    job.setJarByClass(WordCount.class);
-        
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(MapWritable.class); // array
+    job.setOutputKeyClass(Text.class); 
+    job.setOutputValueClass(DoubleWritable.class); 
+    job.setJarByClass(Dalidist.class);
     job.setInputFormatClass(TextInputFormat.class);
     job.setOutputFormatClass(TextOutputFormat.class);
+
+
+    // job.setOutputKeyClass(Text.class);
+    // job.setOutputValueClass(IntWritable.class);
+    // job.setMapOutputValueClass(MapWritable.class); // array 
+    // job.setOutputValueClass(DoubleWritable.class); 
+    // job.setOutputValueClass(FloatWritable.class);    
+    // job.setMapperClass(Map.class);
+    // job.setReducerClass(Reduce.class);
+    // job.setJarByClass(Dalidist.class);
+        
+    // job.setInputFormatClass(TextInputFormat.class);
+    // job.setOutputFormatClass(TextOutputFormat.class);
         
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
